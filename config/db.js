245b3a9c12e1d6ad7c87ff262c823da3c6566029
Gabriel Fernandez <gabe.fer2000@gmail.com>
2020-06-02 12:46:27 -0700

    created routes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); //add mongoose module used for db connection
+const config = require("config"); //add config module
+const db = config.get("mongoURI"); //get values from default json file
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); //if mongoDB fails to connect we want to exit process
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//Create Database connection
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+//define routes
+app.use("/api/users", require("./routes/api/users")); //first arg is path shown in url from start file, second arg is actual file path relative to start file
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
