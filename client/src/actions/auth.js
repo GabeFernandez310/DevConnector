d4b73b53aca226367d771133fcd6a35b4e460db2
Gabriel Fernandez <gabe.fer2000@gmail.com>
2020-06-12 16:48:28 -0700

    Added protected Dashboard route. Added front-end to create, edit, save, and view profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_SUCCESS,
   LOGOUT_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -110,5 +111,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout / Clear profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
